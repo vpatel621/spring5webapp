b53050f544f101a04da8ec3d66afd7ffec183d1c
Viral Patel <vpatel621@gmail.com>
2021-06-30 15:54:57 -0400

    Feat:repositorty

--- src/main/java/guru/springframework/spring5webapp/domain/Author.java
+++ src/main/java/guru/springframework/spring5webapp/domain/Author.java
@@ -19,7 +19,6 @@ public class Author {
     public Author(String firstName, String lastName) {
         this.firstName = firstName;
         this.lastName = lastName;
-
     }
 
     public Long getId() {
